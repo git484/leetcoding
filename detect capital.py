6049c0a61c394f817f6862188f6141f7b8e7b44d
git484 <72640494+git484@users.noreply.github.com>
2020-10-10 09:51:36 +0530

    Add files via upload

---
+++ detect capital.py
@@ -0,0 +1,27 @@
+s=input()
+flag=0
+l=[]
+for x in s[0::]:
+      if s.islower()==True:
+        flag=1
+        print('true')
+        #return True
+        exit()
+
+for x in s:
+     if x.isupper()==True:
+        l.append(1)
+     else:
+        l.append(0)
+if all(l) == True:
+    print(True)
+    flag=1
+    exit()
+
+if s == s.capitalize():
+    print(True)
+    flag=1
+    exit()
+
+if flag==0:
+    print(False)
